1c2e6063a243e93763acc9e3f185c39a88b4bb3c
nrabo4aya <53860750+nrabo4aya@users.noreply.github.com>
2024-01-04 09:27:16 +0200

    Test main api (#9)

---
+++ Dependecies/src/test/java/gds/api/tests/BaseTests.java
@@ -0,0 +1,51 @@
+package gds.api.tests;
+
+import gds.api.Login;
+import gds.api.TripSearch;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BaseTests {
+
+    public static final String USER_NAME = "ITRdevagent";
+    public static final String USER_PASSWORD = "12345678qQ";
+    public static final String BASE_URL = "https://gds-dev1.gillbus.com";
+    public static String token;
+
+
+    @Test
+    public void testLoginInvalid() {
+        Response response = Login.executeLogin(USER_NAME, "password", BASE_URL + "/v2.1/login");
+        assertThat(response.getStatusCode()).isEqualTo(403);
+    }
+
+    @Test
+    public void testLoginValid() {
+        Response response = Login.executeLogin(USER_NAME, USER_PASSWORD, BASE_URL + "/v2.1/login");
+        token = response.getBody().jsonPath().getString("access_token");
+        assertThat(response.getStatusCode()).isEqualTo(200);
+        assertThat(token).isNotNull();
+    }
+
+    @Test
+    public void testSearch() {
+
+        Response loginResponse = Login.executeLogin(USER_NAME, USER_PASSWORD, BASE_URL + "/v2.1/login");
+        token = loginResponse.getBody().jsonPath().getString("access_token");
+        assertThat(loginResponse.getStatusCode()).isEqualTo(200);
+        assertThat(token).isNotNull();
+
+        String fromId = "1611";
+        String toId = "1611";
+        String date = "2023-12-22";
+        String lang = "en";
+        String passCount = "1";
+        String url = BASE_URL + "/search/v2.1/trips";
+
+        Response searchResponse = TripSearch.executeSearch(token, fromId, toId, date, lang, passCount, url);
+        assertThat(searchResponse.getStatusCode()).isEqualTo(204);
+
+    }
+}
